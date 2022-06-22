c38788c2818df6aa4549018921e41e768e7fdded
vat78 <vat78@inbox.ru>
2022-06-22 17:47:43 +0300

    fix test coverage

--- backend/notes-service/build.gradle.kts
+++ backend/notes-service/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.quarkus:quarkus-junit5-mockito")
     testImplementation("io.rest-assured:rest-assured")
+    testImplementation("io.quarkus:quarkus-jacoco")
 }
 
 allOpen {
