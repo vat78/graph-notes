723698a7cb28766b7c907fb9e89ff123d7c96d9e
Viacheslav Tikhonov <vy.a.tikhonov@tinkoff.ru>
2022-09-20 15:38:25 +0300

    add android and web modules

---
+++ clients/web/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.compose.compose
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    js(IR) {
+        browser {
+            testTask {
+                testLogging.showStandardStreams = true
+                useKarma {
+                    useChromeHeadless()
+                    useFirefox()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,36 @@
 rootProject.name = "graph-notes"
 include("backend:notes-service")
+include("clients:android")
+include("clients:web")
 findProject(":backend:notes-service")?.name = "notes-service"
+findProject(":clients:android")?.name = "android"
+findProject(":clients:web")?.name = "web"
 
 pluginManagement{
+
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
     plugins{
         fun String.getVersion() = extra["$this.version"].toString()
         // Kotlin
         kotlin("jvm") version "kotlin".getVersion()
         kotlin("plugin.allopen") version "kotlin".getVersion()
 
+        // Web
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+
         // Quarkus
         id("io.quarkus") version "quarkus".getVersion()
+        id("com.android.application") version "7.2.0"
+
+        // Android
+        id("org.jetbrains.kotlin.android") version "1.7.10"
     }
 }
 
